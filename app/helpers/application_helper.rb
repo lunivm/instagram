791f31e8199d3f9b8a88e27f077881299ed4d0a0
Mykola Luniv <lunivm@yahoo.com>
2023-10-27 00:27:56 +0300

    add svg_icon helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def svg_icon(name, options = {})
+    file_path = "#{Rails.root}/app/assets/images/svg_icons/#{name}.svg"
+    file = Rails.application.assets_manifest.find_sources(file_path).first
+
+    raise "Can't find #{file_path}" unless file
+
+    body   = file.to_s.force_encoding("UTF-8")
+    doc    = Nokogiri::HTML::DocumentFragment.parse body
+    svg    = doc.at_css "svg"
+
+    svg['class'] += " #{options[:class]}"
+
+    raw doc
+  end
 end
