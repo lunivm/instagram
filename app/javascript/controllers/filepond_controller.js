b9ebe7084858f35b7b126f3012d58bf288be8198
Mykola Luniv <lunivm@yahoo.com>
2023-11-08 00:20:54 +0200

    filepond preview; post new fix for modal

--- app/javascript/controllers/filepond_controller.js
+++ app/javascript/controllers/filepond_controller.js
@@ -1,8 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
-import * as FilePond from 'filepond';
+import * as FilePond from 'filepond'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+
+FilePond.registerPlugin(FilePondPluginImagePreview)
 
 export default class extends Controller {
-    connect() {
-        FilePond.create(this.element)
-    }
+  connect() {
+    FilePond.create(this.element, {
+      storeAsFile: true,
+      allowMultiple: true,
+      allowReorder: true
+    })
+  }
 }
