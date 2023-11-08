bf52030bd236b3042af659f4a5a617329abc4f8f
Mykola Luniv <lunivm@yahoo.com>
2023-11-08 10:56:26 +0200

    filepond image validation

--- app/javascript/controllers/filepond_controller.js
+++ app/javascript/controllers/filepond_controller.js
@@ -1,15 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 import * as FilePond from 'filepond'
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
 
-FilePond.registerPlugin(FilePondPluginImagePreview)
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateType
+)
 
 export default class extends Controller {
   connect() {
     FilePond.create(this.element, {
       storeAsFile: true,
       allowMultiple: true,
-      allowReorder: true
+      allowReorder: true,
+      acceptedFileTypes: ['image/*']
     })
   }
 }
