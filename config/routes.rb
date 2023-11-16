edebcbea81989626df1709e5b2d11c9ddc67222d
Mykola Luniv <lunivm@yahoo.com>
2023-11-16 21:44:21 +0200

    add likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
+
   root 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post "toggle_like", to: "likes#toggle_like", as: "toggle_like"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
